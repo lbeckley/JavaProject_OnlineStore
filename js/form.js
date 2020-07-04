d93edc7836482c6d6548776abf1aca56a7128345
Lauren Beckley <lauren.a.beckley@gmail.com>
2020-07-04 02:52:47 -0400

    Fixed form errors

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-  //PHONE NUMBER 
+//PHONE NUMBER 
   $('#telephone').keyup(function(){
     if(this.value !== '' && !isValidPhoneNumber(this.value)) {
       $('#phone-validation-message').show();
@@ -11,7 +11,6 @@
   
   $('#submit').click(function(e){
     e.preventDefault();
-    alert('Server unavaliable, try again later!');
   });
   
   //Checks if phone number is valid.  
@@ -24,7 +23,7 @@
   $('#email-field').keyup(function(){
     if(this.value !== '' && !isValidEmailAddress(this.value)) {
       $('#email-validation-message').show();
-      $('#=submit').prop('disabled', true);
+      $('#submit').prop('disabled', true);
     } else {
       $('#email-validation-message').hide();
       $('#submit').prop('disabled', false);
@@ -33,7 +32,6 @@
   
   $('#submit').click(function(e){
     e.preventDefault();
-    alert('Server unavaliable, try again later!');
   });
   
   //Checks if email address is valid.  
@@ -41,3 +39,17 @@
       var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
       return pattern.test(emailAddress);
   };
+
+  function SubForm (){
+    $.ajax({
+        url:'https://api.apispreadsheets.com/data/680/',
+        type:'post',
+        data:$("#myForm").serializeArray(),
+        success: function(){
+        alert("Thank you for joining ModernRoots mailing list! :)")
+        },
+        error: function(){
+        alert("There was an error :(")
+        }
+    });
+}
