9b26801adfaa30260188afdd6e94d7071de7d010
Lauren Beckley <lauren.a.beckley@gmail.com>
2020-07-04 13:26:50 -0400

    Added countdown to homepage

---
+++ js/timer.js
@@ -0,0 +1,28 @@
+var deadline = new Date("August 1, 2020 0:00:00").getTime(); 
+
+var x = setInterval(function() { 
+var now = new Date().getTime(); 
+var t = deadline - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+
+// Pad the minutes and seconds with leading zeros, if required
+days = ( days < 10 ? "0" : "" ) + days;
+hours = ( hours < 10 ? "0" : "" ) + hours;
+minutes = ( minutes < 10 ? "0" : "" ) + minutes;
+seconds = ( seconds < 10 ? "0" : "" ) + seconds;
+
+document.getElementById("day").innerHTML =days ; 
+document.getElementById("hour").innerHTML =hours; 
+document.getElementById("minute").innerHTML = minutes;  
+document.getElementById("second").innerHTML =seconds;  
+if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "Summer Sale Is Live!"; 
+        document.getElementById("day").innerHTML ='0'; 
+        document.getElementById("hour").innerHTML ='0'; 
+        document.getElementById("minute").innerHTML ='0' ;  
+        document.getElementById("second").innerHTML = '0'; } 
+}, 1000);
